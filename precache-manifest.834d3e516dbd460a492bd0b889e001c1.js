78fe81cdad276e7ddb4220fb1d64e1448a2e258c
alfunsten <alfunsten@gmail.com>
2021-06-23 11:58:56 -0400

    Updates

---
+++ precache-manifest.834d3e516dbd460a492bd0b889e001c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2ecc6bb306fecaa658c55ff64a251a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "2133acacda6e5ab41226",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "06fe3a87831651a093f5",
+    "url": "/home/static/js/2.0f177e26.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.0f177e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2133acacda6e5ab41226",
+    "url": "/home/static/js/main.95ca8f95.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "851645d1bcdc1e3ffcde2af392c706ed",
+    "url": "/home/static/media/anna_funsten.851645d1.jpeg"
+  },
+  {
+    "revision": "541181b059be5f83b0e7c0c623c4b464",
+    "url": "/home/static/media/ga.541181b0.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "ad244fc03cf203baecf4ea5dd182de1e",
+    "url": "/home/static/media/michigan.ad244fc0.png"
+  },
+  {
+    "revision": "7a065a2826cf0158e1c98a67478537c6",
+    "url": "/home/static/media/vanderbilt.7a065a28.png"
+  }
+]);
